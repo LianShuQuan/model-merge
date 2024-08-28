9ce79f92d0f4fb2deceb92e4edda0c0b23032eb1
ErichVonTang <1305697124@qq.com>
2024-08-28 21:11:12 +0000

    update

--- della/CKA_between_models_diagonal.py
+++ della/CKA_between_models_diagonal.py
@@ -13,24 +13,39 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from CKA import CKA, CudaCKA
 import random
+from torch.nn import DataParallel
 from utils.evaluate_llms_utils import generate_instruction_following_task_prompt, generate_code_task_prompt,\
                  read_mbpp, batch_data
-from CKA_between_models_diagonal import get_hidden_states
 
+#  获取每层输出
+def get_hidden_states(model, tokenizer, text, device):
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+    inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    hidden_states = [state.to(device) for state in outputs.hidden_states]  # 将每层的隐藏状态移动到同一设备上
+    return hidden_states
 
+def compute_cka(model1, model2, tokenizer1, tokenizer2, text, device):
+        cuda_cka = CudaCKA(device)
+        hidden_states1 = get_hidden_states(model1, tokenizer1, text, device)
+        hidden_states2 = get_hidden_states(model2, tokenizer2, text, device)
 
-def compute_self_cka(model, tokenizer, text, device):
-    cuda_cka = CudaCKA(device)
-    hidden_states = get_hidden_states(model, tokenizer, text, device)
-    num_layers = len(hidden_states)
-    cka_layer_similarity = torch.zeros((num_layers-1, 1), device=device)
-    for i in range(num_layers-1):
-        with torch.no_grad():
-            feature1 = hidden_states[i].squeeze().float()
-            feature2 = hidden_states[i+1].squeeze().float()
-            cka_similarity = cuda_cka.linear_CKA(feature1, feature2)
-            cka_layer_similarity[i][0] = cka_similarity
-    return cka_layer_similarity
+        num_layers = len(hidden_states1)
+        cka_layer_similarity = torch.zeros((num_layers, num_layers), device=device)
+
+        for i in range(num_layers):
+            for j in range(num_layers):
+                with torch.no_grad():
+                    feature1 = hidden_states1[i].squeeze().float()
+                    feature2 = hidden_states2[j].squeeze().float()
+
+                    # 计算CKA相似度
+                    cka_similarity = cuda_cka.linear_CKA(feature1, feature2)
+                    cka_layer_similarity[i][j] = cka_similarity
+
+        return cka_layer_similarity
 
 parser = argparse.ArgumentParser(description="Generate CKA for MBPP and AlpacaEval")
 parser.add_argument("--model_path1", help="Path to the Model1 checkpoint")
@@ -58,8 +73,12 @@ model2 = AutoModelForCausalLM.from_pretrained(model_path2, cache_dir=cache_dir, 
 
 model1.eval()
 model2.eval()
-device = torch.device('cuda:0')
+device1 = torch.device('cuda:1')
+device2 = torch.device('cuda:2')
 
+# model1.to(device1)
+# model2.to(device2)
+device = torch.device('cuda:0')
 
 if(dataset_name == "alpaca_eval"):
     # 2. 加载数据
@@ -72,14 +91,16 @@ if(dataset_name == "alpaca_eval"):
     for example in dataset:
         instructions.append(example["instruction"])
     text = instructions[random.randint(0, len(instructions))]
-    cka1 = compute_self_cka(model1, tokenizer1, text, device)
-    cka2 = compute_self_cka(model2, tokenizer2, text, device)
-    selected_matrix = torch.cat([cka1,cka2], dim=1).cpu().numpy()
+
+    cka_layer_similarity = compute_cka(model1, model2, tokenizer1, tokenizer2, generate_instruction_following_task_prompt(text), device)
+
+    selected_matrix = cka_layer_similarity.cpu().numpy()
+
 
 elif(dataset_name == "mbpp"):
-    test_data_path = "./code_data/mbpp.test.jsonl"
+    test_data_path = "/home/lsq/della/code_data/mbpp.test.jsonl"
     problems = read_mbpp(test_data_path)
-    task_id = random.randint(11,len(problems)-11)
+    task_id = random.randint(0,len(problems))
     prompt = f"\n{problems[task_id]['text']}\nTest examples:"
     if task_id == 493:
         # The test examples are too long, we choose to only include the function name.
@@ -90,25 +111,37 @@ elif(dataset_name == "mbpp"):
             prompt += f"\n{test_example}"
     prompt = prompt.replace('    ', '\t')
     prompt = generate_code_task_prompt(prompt)
-    cka1 = compute_self_cka(model1, tokenizer1, prompt, device)
-    cka2 = compute_self_cka(model2, tokenizer2, prompt, device)
-    selected_matrix = torch.cat([cka1,cka2], dim=1).cpu().numpy()
+    selected_matrix =  compute_cka(model1, model2, tokenizer1, tokenizer2, prompt, device).cpu().numpy()
 
 
 
 model1_name = model_path1.split("/")[-1]
 model2_name = model_path2.split("/")[-1]
 # 5. 使用Matplotlib和Seaborn绘制热力图
-plt.figure(figsize=(10, 20))
+# plt.figure(figsize=(12, 10))
 
-sns.heatmap(selected_matrix, cbar=True,
-            xticklabels=[model1_name, model2_name],
-            yticklabels=[f'Layer{i}-Layer{i+1}' for i in range(selected_matrix.shape[0])])
+# sns.heatmap(selected_matrix, cbar=True,
+#             xticklabels=[f'Layer {i+1}' for i in range(selected_matrix.shape[1])],
+#             yticklabels=[f'Layer {i+1}' for i in range(selected_matrix.shape[0])])
 
-plt.title('CKA Similarity Matrix between Two Models')
-plt.xlabel(f'model')
-plt.ylabel(f'cka self')
-out_dir = f'cka_fig/cka_self/{dataset_name}'
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-plt.savefig(f'cka_fig/cka_self/{dataset_name}/{model1_name}_{model2_name}_cka_similarity_matrix.png')
+# plt.title('CKA Similarity Matrix between Two Models')
+# plt.xlabel(f'{model1_name} Layers')
+# plt.ylabel(f'{model2_name} Layers')
+# out_dir = f'cka_fig/between_models/{dataset_name}'
+diagonal_elements = np.diag(selected_matrix)
+plt.figure(figsize=(12, 6))
+
+# 绘制柱状图
+plt.bar(range(1, len(diagonal_elements) + 1), diagonal_elements)
+
+# 添加标题和轴标签
+plt.title(f'CKA Similarity between {model1_name} and {model2_name}_{dataset_name}')
+plt.xlabel('Layer')
+plt.ylabel('CKA Similarity')
+
+# 设置输出目录
+out_dir = f'cka_fig/between_models/{dataset_name}'
+
+# if not os.path.exists(out_dir):
+#     os.makedirs(out_dir)
+plt.savefig(f'CKA_similarity/cka_similarity_{model1_name}_{model2_name}_{dataset_name}.png')
